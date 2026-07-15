61a7a6e1a23d0d1e83508ce806276bc5e9d6da0c
Thibault Bondetti <thibault.bondetti@cgi.com>
2026-07-15 15:51:20 +0200

    [feat] Initialisation via Spring Initializr

---
+++ src/test/java/io/github/tbondetti/authserver/AuthServerApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.tbondetti.authserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuthServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
