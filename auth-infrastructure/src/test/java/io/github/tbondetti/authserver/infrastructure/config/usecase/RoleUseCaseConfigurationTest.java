0644952fd5bf472fe9e42ed4810ef0d1f6ba85f5
Thibault Bondetti <thibault.bondetti@cgi.com>
2026-07-22 14:50:06 +0200

    [feat] TU

--- auth-infrastructure/src/test/java/io/github/tbondetti/authserver/infrastructure/config/usecase/RoleUseCaseConfigurationTest.java
+++ auth-infrastructure/src/test/java/io/github/tbondetti/authserver/infrastructure/config/usecase/RoleUseCaseConfigurationTest.java
@@ -3,6 +3,7 @@ package io.github.tbondetti.authserver.infrastructure.config.usecase;
 import io.github.tbondetti.authserver.core.port.RoleRepositoryPort;
 import io.github.tbondetti.authserver.core.usecase.application.GetApplicationUseCase;
 import io.github.tbondetti.authserver.core.usecase.role.CreateRoleUseCase;
+import io.github.tbondetti.authserver.core.usecase.role.DeleteRoleUseCase;
 import io.github.tbondetti.authserver.core.usecase.role.GetRoleUseCase;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -25,6 +26,9 @@ class RoleUseCaseConfigurationTest {
     @Mock
     private GetApplicationUseCase getApplicationUseCase;
 
+    @Mock
+    private GetRoleUseCase getRoleUseCase;
+
     @Test
     void createRoleUseCaseShouldReturnCreateRoleUseCase() {
         final CreateRoleUseCase actual = this.subject.createRoleUseCase(
@@ -45,5 +49,16 @@ class RoleUseCaseConfigurationTest {
 
         assertNotNull(actual);
         assertInstanceOf(GetRoleUseCase.class, actual);
+    }
+
+    @Test
+    void deleteRoleUseCaseShouldReturnDeleteRoleUseCase() {
+        final DeleteRoleUseCase actual = this.subject.deleteRoleUseCase(
+                this.roleRepositoryPort,
+                this.getRoleUseCase
+        );
+
+        assertNotNull(actual);
+        assertInstanceOf(DeleteRoleUseCase.class, actual);
     }
 }
